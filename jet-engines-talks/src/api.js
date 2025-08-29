dbefdfdd2ac0fff34a13ba9902c42abbb4d0bb27
Ajatshatru Singh <ajatshatrusingh@Ajatshatrus-MacBook-Air.local>
2025-08-29 19:45:37 +0530

    api.js  change2

--- jet-engines-talks/src/api.js
+++ jet-engines-talks/src/api.js
@@ -1,5 +1,7 @@
   
-const API_BASE_URL = import.meta.env.VITE_API_URL; 
- 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-export default API_BASE_URL;
+export const fetchPosts = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/posts`);
+  return response.json();
+};
